32169ca6a657eba7dc9855d1a0470063fcdeecb0
yudigf <yudigf08@gmail.com>
2025-09-12 10:12:50 +0700

    feat: add database migrations for roles, users, categories, items, orders, and order items

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,12 +13,16 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('username')->unique()->fillable();
+            $table->string('password')->nullable();
+            $table->string('fullname');
+            $table->string('email')->unique()->fillable();
+            $table->string('phone');
+            $table->unsignedBigInteger('role_id');
+            $table->softDeletes();
             $table->timestamps();
+
+            $table->foreign('role_id')->references('id')->on('roles');
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
